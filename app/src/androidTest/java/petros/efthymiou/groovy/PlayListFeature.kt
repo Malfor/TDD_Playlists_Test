81aea835530d9e423a35b2d4041e330284eb8658
robarrios <rodolfo.barrios@mercadolibre.com.co>
2022-02-11 11:50:51 -0500

    base project

--- app/src/androidTest/java/petros/efthymiou/groovy/PlayListFeature.kt
+++ app/src/androidTest/java/petros/efthymiou/groovy/PlayListFeature.kt
@@ -1,24 +1,71 @@
 package petros.efthymiou.groovy
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.schibsted.spain.barista.assertion.BaristaRecyclerViewAssertions.assertRecyclerViewItemCount
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.schibsted.spain.barista.internal.matcher.DrawableMatcher.Companion.withDrawable
+import java.util.EnumSet.allOf
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class PlayListFeature {
+
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+        @Rule get
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("petros.efthymiou.groovy", appContext.packageName)
+    fun displayScreenTitle() {
+        assertDisplayed("Playlists")
+    }
+
+    @Test
+    fun displayListOfPlaylists() {
+        assertRecyclerViewItemCount(R.id.playlists_list, 10)
+
+        onView(allOf(withId(R.id.playlist_name), isDescendantOfA(withId(R.id.playlists_list)), 0))
+            .check(matches(withText("Al Deckow")))
+            .check(matches(isDisplayed()))
+
+        onView(allOf(withId(R.id.playlist_category), isDescendantOfA(withId(R.id.playlists_list)), 0))
+            .check(matches(withText("Rap")))
+            .check(matches(isDisplayed()))
+
+        onView(allOf(withId(R.id.playlist_image), isDescendantOfA(withId(R.id.playlists_list)), 0))
+            .check(matches(withDrawable(R.mipmap.playlist)))
+            .check(matches(isDisplayed()))
+    }
+
+    fun nthChildOf(parentMatcher: Matcher<View>, childPosition: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("position $childPosition of parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                if (view.parent !is ViewGroup) return false
+                val parent = view.parent as ViewGroup
+
+                return (parentMatcher.matches(parent)
+                        && parent.childCount > childPosition
+                        && parent.getChildAt(childPosition) == view)
+            }
+        }
     }
 }
